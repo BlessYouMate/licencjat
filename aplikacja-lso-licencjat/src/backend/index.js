6540481b50f004a9e1d771dabaa6eac0f1c4d88c
Wiktor <wiktorjck@gmail.com>
2025-03-07 23:50:01 +0100

    adding prefix api do router

--- aplikacja-lso-licencjat/src/backend/index.js
+++ aplikacja-lso-licencjat/src/backend/index.js
@@ -18,7 +18,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(authRouter);
+app.use("/api", authRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
