68614e200d616367ad8ebc1df30254190c6587e5
Wiktor <wiktorjck@gmail.com>
2025-03-07 20:21:32 +0100

    trying to use env variables from railway database

--- aplikacja-lso-licencjat/src/backend/config/db.js
+++ aplikacja-lso-licencjat/src/backend/config/db.js
@@ -3,36 +3,41 @@ import dotenv from 'dotenv';
 
 dotenv.config({ path: './.env' });
 
-const { Pool } = pkg
+const { Pool } = pkg;
+
+console.log("Database URL:", process.env.DATABASE_URL);
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL, 
-    ssl: { rejectUnauthorized: false } // Railway wymaga SSL
+    connectionString: process.env.DATABASE_URL, // Zmienna z Railway
+    ssl: {
+        rejectUnauthorized: false, // Railway wymaga SSL
+    },
 });
 
+
+
 const createTables = async () => {
-    try{
-        const CreateUserTable = 
-        `
+    try {
+        const CreateUserTable = `
         CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         login VARCHAR(16) UNIQUE NOT NULL,
         password VARCHAR(128) NOT NULL
         );
-    `;
+        `;
 
-    await pool.query(CreateUserTable);
-    console.log("User table successfully created or previously existed")
+        await pool.query(CreateUserTable);
+        console.log("User table successfully created or previously existed");
+    } catch (err) {
+        console.log("Error creating tables:", err);
     }
-    catch(err){
-        console.log("error creating tables", err)
-    }
-}
+};
 
 pool.connect()
-  .then(() => console.log("Połączono z bazą danych PostgreSQL"))
-  .catch(err => console.error("Błąd połączenia z bazą:", err));
+    .then(() => {
+        console.log("Connected to PostgreSQL database");
+        createTables();
+    })
+    .catch(err => console.error("Database connection error:", err));
 
-createTables();
-
-export default pool
+export default pool;
